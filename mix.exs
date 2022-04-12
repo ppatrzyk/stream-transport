8ef21f882f6ea8092aa3f7228d9f9675b4d2a0cc
Piotr Patrzyk <piotr@patrzyk.me>
2022-04-12 16:27:50 +0200

    release config

--- mix.exs
+++ mix.exs
@@ -7,7 +7,12 @@ defmodule StreamTransport.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      releases: [
+        my_release: [
+          include_executables_for: [:unix],
+        ],
+      ],
     ]
   end
 
